e1c66325d86cc066dd441926868fa6ee54448ff2
Gabriel Souto Maracajá <gabriel@MacBook-Air-de-Gabriel.local>
2019-04-05 00:39:13 -0300

    Finalizando o Lab

--- ri_lab_01/spiders/brasil_247.py
+++ ri_lab_01/spiders/brasil_247.py
@@ -17,15 +17,28 @@ class Brasil247Spider(scrapy.Spider):
                 data = json.load(json_file)
         self.start_urls = list(data.values())
 
+    def propensity_url(self, response):
+        
+        know = yield {
+            'titulo': response.xpath('//*[@id="wrapper"]/div[5]/h1/text()').get(),
+            'subtitulo': response.xpath('//*[@id="wrapper"]/div[5]/p[2]/text()').get(),
+            'autor': response.xpath('//*[@id="wrapper"]/div[6]/section[1]/div[1]/p[2]/strong/text()').get(),
+            'data': response.xpath('//*[@id="wrapper"]')[0].css('p::text')[2].get(),
+            'seccao': response.css('body::attr(id)').get().split("-")[-1],
+            'texto': response.xpath('//*[@id="wrapper"]/div[6]')[0].css('p::text').getall(),
+            'url': response.url
+        }
+
     def parse(self, response):
-        #
-        # inclua seu código aqui
-        #
+
+        links = response.css('article a::attr(href)').getall()
+
+        for href in links:
+            yield response.follow(href, callback = self.propensity_url
+    )
+    
         page = response.url.split("/")[-2]
         filename = 'quotes-%s.html' % page
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
-        #
-        #
-        #
